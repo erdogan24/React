47a7415c57e9a3ecfaea2116cee18483d1ea990c
Erdoğan <77540591+erdogan24@users.noreply.github.com>
2021-06-06 00:57:40 +0300

    unit2

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import ProductList from '../pages/ProductList'
-import Categories from './Categories'
-import Navi from './Navi'
+import React from "react";
+import ProductList from "../pages/ProductList";
+import Categories from "./Categories";
+import Navi from "./Navi";
+import { Grid, GridColumn } from "semantic-ui-react";
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Navi/>
-            <Categories/>
-            <ProductList/>
-        </div>
-    )
+  return (
+    <div>
+      <Grid>
+        <Grid.Row>
+          <GridColumn width={4}>
+            <Categories />
+          </GridColumn>
+          <GridColumn width={12}>
+            <ProductList />
+          </GridColumn>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React, {useState} from "react";
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 export default function ProductList() {
+  
+  const [state, setstate]  = useState([])
+  
     return (
-        <div>
-            Ürünler
-        </div>
-    )
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Ürün Adı</Table.HeaderCell>
+            <Table.HeaderCell>Birim fiyatı</Table.HeaderCell>
+            <Table.HeaderCell>Stok adedi</Table.HeaderCell>
+            <Table.HeaderCell>Açıklama</Table.HeaderCell>
+            <Table.HeaderCell>Kategori </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <Label ribbon>First</Label>
+            </Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="3">
+              <Menu floated="right" pagination>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a">1</Menu.Item>
+                <Menu.Item as="a">2</Menu.Item>
+                <Menu.Item as="a">3</Menu.Item>
+                <Menu.Item as="a">4</Menu.Item>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </div>
+  );
 }
