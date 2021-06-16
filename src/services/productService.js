b294aec316a3a2e2bf7bfe782ff2b31943adc937
Erdoğan <77540591+erdogan24@users.noreply.github.com>
2021-06-16 22:44:17 +0300

    16 haziran 22.42

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export default class ProductService {
     getProducts() {
-        return axios.get("")
+        return axios.get("http:localhost:8080")
     }
 }
---
+++ src/store/reducers/cartReducer.js
@@ -0,0 +1,18 @@
+import { ADD_TO_CART } from "../actions/cartActions";
+import { cartItems } from "../initialValues/cartItems";
+
+
+const initialState = {
+   cartItems:cartItems
+}
+
+export default function cartReducer(state=initialState,{type,payload} ) {
+    switch (key) {
+        case ADD_TO_CART:
+            let product = state.cartItems.find(c=>c.product.id===payload.id)
+            break;
+    
+        default:
+            break;
+    }
+}
